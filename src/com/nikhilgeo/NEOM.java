9a92f135eded47a4f3f6d67f58f9a432604570da
Nikhil George <nikhilgeo@gmail.com>
2015-02-15 00:39:54 +0530

    inode lookup functions called, space replacement issue resolved

--- src/com/nikhilgeo/NEOM.java
+++ src/com/nikhilgeo/NEOM.java
@@ -6,12 +6,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class NEOM {
     static String tcpConnections, tcpCon, pattern;
-    static String rem_IP, rem_port, local_IP, local_port, UID, inode, tcpConStatus, timestamp;
+    static String rem_IP, rem_port, local_IP, local_port, UID, inode, tcpConStatus, timestamp, pid, processName;
     static int conStatusCode;
+    static Utilities utilities = new Utilities();
+    static Inode_uid_process_Maping inode_uid_process_maping = new Inode_uid_process_Maping();
 
 
     static enum tcp_status { /* As defined in ./include/net/tcp_states.h */
@@ -31,57 +35,61 @@ public class NEOM {
 
 
     public static void main(String[] args) throws IOException {
-        /* Uncomment ALL
-        Utilities utilities = new Utilities();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SS");
         Date dt = new Date();
-        System.out.println("Time: " + dateFormat.format(dt));
-
+        System.out.println("Start Time: " + dateFormat.format(dt));
 
         tcpConnections = utilities.readFile_InOneGO("/proc/net/tcp");
         if (tcpConnections != "") {
             timestamp = dateFormat.format(dt);
+            inode_uid_process_maping.get_pid_inode_processName(); //Initialize the HashTable
             processFile(tcpConnections, timestamp);
         } else {
             System.out.println("No data from File read");
         }
-        System.out.println("Time: " + dateFormat.format(dt));
-*/
-
-
-        Inode_uid_process_Maping obj = new Inode_uid_process_Maping();
-        obj.get_pid_inode_processName();
-
+        System.out.println("End Time: " + dateFormat.format(dt));
 
         //System.out.println("eachTcpConnections=" + tcpConArray[1]);
         //System.out.println("Default Charset=" + Charset.defaultCharset());
-
-
     }
 
     private static void processFile(String Connections, String timestamp) {
+
         System.out.print(Connections);
+        List<String> pid_pname = new ArrayList<String>();
         String tcpConArray[] = Connections.split("\n"); //Split to line by line
+
+
+        // To print hashtable before lookup, just to make sure hashtable is intact
+        inode_uid_process_maping.print_HashTable_getInode_pid_pname_mapping();
+        System.out.println("------------------After processing----------");
+        System.out.println("timestamp" + "|" + "local_IP" + "|" + "local_port" + "|" + "rem_IP" + "|" + "rem_port" + "|" + "tcpConStatus" + "|" + "inode" + "|" + "pid" + "|" + "processName");
+
         //totTCPCon = 1 because excluded the header
         //Loop through each line
-        System.out.println("After processing");
-
         for (int tcpConIndex = 1; tcpConIndex < tcpConArray.length; tcpConIndex++) {
+            //System.out.println(timestamp + "|" + local_IP + "|" + local_port + "|" + rem_IP + "|" + rem_port + "|" + tcpConStatus + "|" + inode + "|" + UID);
+
             tcpCon = tcpConArray[tcpConIndex];
-            pattern = "(\\s\\s)+"; // reg pattern to match two or more spaces
-            tcpCon = tcpCon.replaceAll(pattern, " ").trim(); //replace all multiple spaces
+            pattern = "(\\s\\s)+"; // reg pattern to match even number of spaces
+            tcpCon = tcpCon.replaceAll(pattern, " ").trim(); //replace all even no of spaces with single space
+            tcpCon = tcpCon.replaceAll(pattern, " ").trim(); //replace the even no of spaces created coz of above
             //System.out.println("tcpCon=" + tcpCon);
             String tcpIndividualConn[] = tcpCon.split(" "); //split each field by space
 
+            //System.out.println("Length of tcpIndividualConn= " + tcpIndividualConn.length);
+            //System.out.println(timestamp + "|" + local_IP + "|" + local_port + "|" + rem_IP + "|" + rem_port + "|" + tcpConStatus + "|" + inode + "|" + UID);
+
+
             String local_SocketHEX[] = tcpIndividualConn[1].split(":"); //split Local IP:Port
-            local_IP = little_endianIP_to_decimal(local_SocketHEX[0]);
-            local_port = hex_to_decimal(local_SocketHEX[1]);
+            local_IP = utilities.little_endianIP_to_decimal(local_SocketHEX[0]);
+            local_port = utilities.hex_to_decimal(local_SocketHEX[1]);
 
             String rem_addressHEX[] = tcpIndividualConn[2].split(":"); //split Remote IP:Port
-            rem_IP = little_endianIP_to_decimal(rem_addressHEX[0]);
-            rem_port = hex_to_decimal(rem_addressHEX[1]);
+            rem_IP = utilities.little_endianIP_to_decimal(rem_addressHEX[0]);
+            rem_port = utilities.hex_to_decimal(rem_addressHEX[1]);
 
-            conStatusCode = Integer.parseInt(hex_to_decimal(tcpIndividualConn[3]));
+            conStatusCode = Integer.parseInt(utilities.hex_to_decimal(tcpIndividualConn[3]));
 
             //Not optimized | performance degradation
             //as arrays are mutable, values() must return a copy of the array of elements just in case you happen to change it.
@@ -91,36 +99,26 @@ public class NEOM {
             UID = tcpIndividualConn[7];
             inode = tcpIndividualConn[9];
 
-            System.out.println("timestamp" + " " + "local_IP" + " " + "local_port" + " " + "rem_IP" + " " + "rem_port" + " " + "tcpConStatus");
 
-            System.out.println(timestamp + "|" + local_IP + "|" + local_port + "|" + rem_IP + "|" + rem_port + "|" + tcpConStatus);
+            //System.out.println("Inode for lookup =" + inode);
+            pid_pname = inode_uid_process_maping.pid_processName_lookup(inode);
+            //System.out.println(pid_pname);
+            if (pid_pname != null) {
+
+                pid = pid_pname.get(0);
+                //System.out.println("pid" + pid);
+                processName = pid_pname.get(1);
+                //System.out.println("processName" + processName);
+
+            }
+            //System.out.println("timestamp" + " " + "local_IP" + " " + "local_port" + " " + "rem_IP" + " " + "rem_port" + " " + "tcpConStatus");
+
+            System.out.println(timestamp + "|" + local_IP + "|" + local_port + "|" + rem_IP + "|" + rem_port + "|" + tcpConStatus + "|" + inode + "|" + pid + "|" + processName);
             //System.out.println("tcpConFieldsLength=" + tcpCon);
             //System.out.println("tcpConFieldsLength=" + tcpConFields.length);
             //String  local_addressHEX = tcpConFields[1];
         }
     }
 
-    private static String hex_to_decimal(String hex) {
-        int decimal;
-        decimal = Integer.parseInt(hex, 16);
-        return String.valueOf(decimal);
-    }
 
-    static String little_endianIP_to_decimal(String little_endian_IP) {
-        //System.out.println("little_endian_IP =" + little_endian_IP);
-        String octect, IPAddr;
-        int decimal;
-        StringBuilder reversed_IP = new StringBuilder("");
-
-        for (int index = little_endian_IP.length() - 1; index >= 0; index = index - 2) {
-            octect = little_endian_IP.substring(index - 1, index + 1);
-            //System.out.println("octect" + octect);
-            decimal = Integer.parseInt(octect, 16);
-            //System.out.println("decimal" + decimal);
-            reversed_IP = reversed_IP.append(decimal);
-            reversed_IP = reversed_IP.append(":");
-        }
-        IPAddr = reversed_IP.toString();
-        return IPAddr.substring(0, IPAddr.length() - 1);
-    }
 }
